d43122741b6668095e102ebe17d8c0f7d2ddb94c
BERNARDO NILSON <bernardo.nilson@edu.pucrs.br>
2023-06-02 13:06:07 -0300

    Classroom update

---
+++ MaxCommunDivisor.java
@@ -0,0 +1,20 @@
+
+/**
+ * Undertanding the problem: Write a program that 
+ * 
+ * @author: Bernardo Nilson
+ * @version: 02.06.2023
+ */
+
+ public class MaxCommunDivisor {
+
+    public static void main(String[] args) {
+
+        // Clean the terminal 
+        System.out.print("\033\143");
+        System.out.println("Hey, this program XXXX!\n");
+
+        
+        // END
+    }
+}
